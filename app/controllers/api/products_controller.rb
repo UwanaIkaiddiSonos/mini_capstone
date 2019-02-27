cb0adda8b17a0ebdd5efdcc07629599ee12f5f67
Uwana <uwikaiddi@gmail.com>
2019-02-26 20:04:27 -0600

    Add json jbuilder

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,4 @@
 class Api::ProductsController < ApplicationController
+  @products = Product.all
+  render 'all_the_products.json.jbuilder'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+  get '/all_products' => 'products#every_product'
 end
