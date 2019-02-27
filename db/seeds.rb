fb448ae1a5fc3691e8e558494f1fdb179d4363ef
Uwana <uwikaiddi@gmail.com>
2019-02-26 19:49:49 -0600

    Add products; migrate db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product1 = Product.new(name = "wrench", price = 24, image_url = "#", description = "I think you turn things with this." )
+
+product1.save
+
+product2 = Product.new(name = "bucket", price = 5, image_url = "#", description = "Gets lighter when you put a hole in it.")
+
+product2.save
+
+product3 = Product.new(name = "fishing_pole", price = 39, image_url = "#", description = "Use it to defeat Old Man Ganondorf")
