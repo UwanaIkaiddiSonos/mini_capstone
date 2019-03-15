e95b138f634654cd1d044f0e02ddc11e04e4fd75
Uwana <uwikaiddi@gmail.com>
2019-03-15 15:21:58 -0500

    Add create, show, patch, and destroy routes

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -3,4 +3,39 @@ class Api::ProductsController < ApplicationController
     @products = Product.all
     render 'index.json.jbuilder'
   end
+
+  def create
+    @product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description]
+    )
+    @product.save
+    render 'show.json.jbuilder'
+  end
+
+  def show
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    @product.name = params[:name]
+    @product.price = params[:price]
+    @product.image_url = params[:image_url]
+    @product.description = params[:description]
+    @product.save
+    render 'show.json.jbuilder'
+  end
+
+  def destroy
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+    @product.destroy
+    render 'destroy.json.jbuilder'
+  end
 end
